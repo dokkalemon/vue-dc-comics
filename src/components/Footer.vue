4e40d7b1b85b01332ecdaf1acf084dcd94d11b32
dokkalemon <dottori.roberto@gmail..com>
2021-11-23 15:41:24 +0100

    reset footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,5 @@
 <template>
-    <footer class="full-width">
-       <div class="footer full-width debug">
-           <img src="@/assets/footer-bg.jpg" alt="">
-       </div>
-       <div class="footer-button">
-
-       </div>
-    </footer>
+ 
 
 
 
@@ -31,26 +24,6 @@ export default {
 <style lang="scss">
 @import '@/styles/variables';
 
-footer {
-    height: 500px;
-
-    .footer {
-        height: 100%;
-
-        img {
-            width: 100%;
-            object-fit: cover;
-        }
-    }
-
-
-    .footer-button {
-        height: 150px;
-        background-color: $secondary-color
-    }
-
-   
-}
 
 
 </style>
